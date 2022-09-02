64e074ac5f344b7cf34c203208ba427a19754ffb
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2022-09-02 11:37:33 +0600

    Update RepoCard.tsx

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -4,43 +4,51 @@ import {useActions} from '../hooks/actions'
 import {useAppSelector} from '../hooks/redux'
 
 export function RepoCard({ repo }: { repo: IRepo }) {
-  const {addFavourite, removeFavourite} = useActions()
-  const {favourites} = useAppSelector(state => state.github)
+    const {addFavourite, removeFavourite} = useActions()
+    const {favourites} = useAppSelector(state => state.github)
 
-  const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
-  
-  const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-    addFavourite(repo.html_url)
-    setIsFav(true)
-  }
+    const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
 
-  const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-    removeFavourite(repo.html_url)
-    setIsFav(false)
-  }
-  
-  return (
-    <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
-      <a href={repo.html_url} target="_blank">
-        <h2 className="text-lg font-bold">{repo.full_name}</h2>
-        <p className="text-sm">
-          Forks: <span className="font-bold mr-2">{repo.forks}</span>
-          Watchers: <span className="font-bold">{repo.watchers}</span>
-        </p>
-        <p className="text-sm font-thin">{repo?.description}</p>
+    const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        addFavourite(repo.html_url)
+        setIsFav(true)
+    }
 
-        {!isFav && <button
-          className="py-2 px-4 bg-yellow-400 mr-2 rounded hover:shadow-md transition-all"
-          onClick={addToFavourite}
-        >Add</button>}
+    const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        removeFavourite(repo.html_url)
+        setIsFav(false)
+    }
 
-        {isFav && <button
-          className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
-          onClick={removeFromFavourite}
-        >Remove</button>}
-      </a>
-    </div>
-  )
+    return (
+        <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
+            <a href={repo.html_url} target="_blank">
+                <h2 className="text-lg font-bold">{repo.full_name}</h2>
+                <p className="text-sm">
+                    Forks: <span className="font-bold mr-2">{repo.forks}</span>
+                    Watchers: <span className="font-bold">{repo.watchers}</span>
+                </p>
+                <p className="text-sm font-thin">{repo?.description}</p>
+
+                {!isFav && <button
+                    className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={addToFavourite}
+                >Add
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-2 w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+                    </svg>
+                </button>}
+
+                {isFav && <button
+                    className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={removeFromFavourite}
+                >Remove
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-2 w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                    </svg>
+                </button>}
+            </a>
+        </div>
+    )
 }
